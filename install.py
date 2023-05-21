bbdad3104aca9c92d2a5d866d15a86f17fa11b45
maxixa <59010137+maxixa@users.noreply.github.com>
2023-05-21 07:56:26 +0700

    Update install.py

--- install.py
+++ install.py
@@ -4,15 +4,6 @@ packages = {
     "chardet": "chardet",
     "fastapi": "fastapi",
     "hashlib": "hashlib",
-
-    # The following packages are required for translation service. If you do not need translation service, you can remove them.
-    # 以下是翻译所需的包，如果不需要翻译服务，可以删除掉它们。
-    "translators": "translators",
-    "openai": "openai",
-    "boto3": "boto3",
-    "aliyunsdkcore": "aliyun-python-sdk-core",
-    "aliyunsdkalimt": "aliyun-python-sdk-alimt",
-    "tencentcloud": "tencentcloud-sdk-python",
 }
 
 for package_name in packages:
